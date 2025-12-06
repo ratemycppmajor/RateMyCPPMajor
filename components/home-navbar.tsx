e08bd6fa7c59a89f93b2e6c3f563fdb77f3ce19c
Brandon Diep <brandondiep2711@gmail.com>
2025-12-05 16:50:33 -0800

    add hero to homepage

--- components/home-navbar.tsx
+++ components/home-navbar.tsx
@@ -44,7 +44,7 @@ export default function HomeNavbar() {
 
   return (
     <header
-      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${isScrolled ? 'border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60' : 'bg-transparent'}`}
+      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${isScrolled ? `${window.scrollY > 100 && "border-b"} bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60` : 'bg-transparent'}`}
     >
       <nav className={`max-w-7xl mx-auto px-4 sm:px-6 lg:px-8`}>
         <div className="flex items-center justify-between h-16">
